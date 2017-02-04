59713d76e9d23ab9e9b849419cb4d24618543c23
Kristin Heininger <dauphindouze@comcast.net>
2017-02-04 23:46:41 +0000

    Added courses index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :courses, only: [:new, :create, :show, :destroy]
   end
+  resources :courses, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
