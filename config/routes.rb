76e01077ef5912665597792a78ab99a7d95cdfc3
Kristin Heininger <dauphindouze@comcast.net>
2017-02-06 03:47:51 +0000

    Added lessons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show, :destroy] do
       resources :sections, only: [:new, :create]
     end
