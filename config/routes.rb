26973f74a099a210a9955d1c3f44887d67e63aa3
Kristin Heininger <dauphindouze@comcast.net>
2017-02-09 04:50:38 +0000

    Added lesson show page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     end
   end
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
