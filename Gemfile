4bbc86c0dd39a727f64bd1ba06e51a5cd057a378
Kristin Heininger <dauphindouze@comcast.net>
2017-03-04 20:39:51 +0000

    Added stripe for payments

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ gem 'fog'
 gem 'mini_magick'
 
 gem 'carrierwave-aws'
+
+gem 'stripe'
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
   end
@@ -9,4 +11,11 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to courses_path(current_lesson.section.course), alert: 'Not enrolled in the course'
+    end
+  end
+
 end
